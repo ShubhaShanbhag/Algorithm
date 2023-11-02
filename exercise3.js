c3c55203768e661ebf1b052d2189e0890b8f31f4
Shubha <145449009+ShubhaShanbhag@users.noreply.github.com>
2023-11-02 21:45:57 +0100

    Add files via upload

---
+++ exercise3.js
@@ -0,0 +1,61 @@
+//Exercise 3
+
+//Solve the following exercises and submit your answers on GitHub under function_exercises.
+
+// Exercise 1
+
+// The following function returns true if the parameter age is greater than 18.
+
+// Otherwise, it asks for a confirmation and returns its result.
+//JAVASCRIPT
+// function checkAge(age) {
+  //   if (age > 18) {
+  // return true;
+  // } else {
+   //  return confirm('Do you have your parents permission to access this page?');
+ //  }
+// }
+// Rewrite it, to perform the same, but without if...else, and using arrow function.
+
+
+
+
+
+// Exercise 2
+
+// Write a function pow(x,n) that returns x in power n. Or, in other words, multiplies x by itself n times and returns the result.
+
+
+
+// Exercise 3
+
+// Replace Function Expressions with arrow functions in the code:
+
+// JAVASCRIPT
+// function ask(question, yes, no) {
+//   if (confirm(question)) yes()
+//   else no()
+// }
+
+// ask(
+//   "Do you agree?",
+//   function() { alert("You agreed.") },
+//   function() { alert("You canceled the execution.") }
+// )
+
+
+
+
+// Exercise 4
+
+// Create an object calculator with three methods:
+
+// read() prompts for two values and saves them as object properties.
+// sum() returns the sum of saved values.
+// mul() multiplies saved values and returns the result.
+
+
+
+// Exercise 5
+
+// Write a function min(a,b) which returns the least of two numbers a and b. Use arrow function along with the question mark operator ?
